cad0ebb25f1c7bed19b3f0810983c47a93b88c9e
Jonathan Chiou <jonathan.chiou@veeeva.com>
2016-08-15 12:37:13 -0700

    Got genymotion set up at work. Pressing the up button now finishes the register/login activity.

--- app/src/main/java/bachmanity/prank_call/Activities/RegisterActivity.java
+++ app/src/main/java/bachmanity/prank_call/Activities/RegisterActivity.java
@@ -3,6 +3,7 @@ package bachmanity.prank_call.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import bachmanity.prank_call.Misc.Constants;
@@ -24,5 +25,17 @@ public class RegisterActivity extends AppCompatActivity {
         if (intent.getBooleanExtra(Constants.firstTime, false)) {
             SnackbarHelper.showSnackbar(this, parent, Constants.firstTimeMsg);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
